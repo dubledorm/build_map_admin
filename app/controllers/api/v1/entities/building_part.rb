675c455c51b19cc39283bb0b4106248a23b4f07e
ubuntu <dubledorm@gmail.com>
2024-01-03 09:58:31 -0500

    Исправлен Api:V1:Entities:BuildingPart Теперь возвращается immutable_map_url

--- app/controllers/api/v1/entities/building_part.rb
+++ app/controllers/api/v1/entities/building_part.rb
@@ -7,12 +7,12 @@ class Api
       class BuildingPart < Grape::Entity
         expose :name, documentation: { type: String, desc: 'Название' }
         expose :map_scale, documentation: { type: String, desc: 'Маштаб карты' }
-        expose :original_map_url, documentation: { type: String, desc: 'Карта' }
+        expose :immutable_map_url, documentation: { type: String, desc: 'Карта' }
 
         private
 
-        def original_map_url
-          Rails.application.routes.url_helpers.rails_blob_path(object.original_map, only_path: true)
+        def immutable_map_url
+          Rails.application.routes.url_helpers.rails_blob_path(object.immutable_map, only_path: true)
         end
       end
     end
